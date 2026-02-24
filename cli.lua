7346d0da52039ad3a2798eb22f14159af7f4176a
TheMCNetwork <gamingw8184@gmail.com>
2026-02-24 13:13:26 +0100

    Initial Commit

---
+++ cli.lua
@@ -0,0 +1,18 @@
+CreateThread(function()
+    RequestStreamedTextureDict("squaremap", false) -- the custom ytd we have in ./stream/
+
+    while not HasStreamedTextureDictLoaded("squaremap") do
+        Wait(50)
+    end
+
+    AddReplaceTexture("platform:/textures/graphics", "radarmasksm", "squaremap", "radarmasksm")
+    AddReplaceTexture("platform:/textures/graphics", "radarmasklg", "squaremap", "radarmasklg")
+end)
+
+AddEventHandler("onResourceStop", function(resourceName) -- remove the replaced textures on resource restart
+    if resourceName == GetCurrentResourceName() then
+        RemoveReplaceTexture("platform:/textures/graphics", "radarmasksm")
+        RemoveReplaceTexture("platform:/textures/graphics", "radarmasklg")
+        SetMinimapComponent(15, false, 0)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+
+fx_version 'cerulean'
+
+game 'gta5'
+description 'Square Minimap with no blur mask'
+
+files {
+    'stream/*.ytd'
+}
+
+data_file 'TEXTURE_DICTIONARY' 'stream/*.ytd'
